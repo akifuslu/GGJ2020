5a09b3248bf517a3b135de5250777ccad86a0889
akifuslu <40760783+akifuslu@users.noreply.github.com>
2020-01-31 22:06:56 +0300

    Movement basics

---
+++ ggj2020/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    public float MoveScalar;
+    public float JumpScalar;
+    public float FallScalar;
+    public Transform[] GroundPivots;
+
+    private bool _isGrounded;
+    private Rigidbody _body;
+    private SpriteRenderer _sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _body = GetComponent<Rigidbody>();
+        _sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        _isGrounded = CheckGrounded();
+        float hort = Input.GetAxis("Horizontal");
+        var velocity = _body.velocity;
+        velocity.x = hort * MoveScalar;
+        if(Input.GetKey("w") && _isGrounded)
+        {
+            velocity.y = JumpScalar;
+        }
+        if(!_isGrounded && velocity.y < 0)
+        {
+            velocity.y -= FallScalar;
+        }
+        _body.velocity = velocity;
+
+        //set character facing side
+        if(hort > 0)
+        {
+            _sprite.flipX = false;
+        }
+        else if(hort < 0)
+        {
+            _sprite.flipX = true;
+        }
+
+    }
+
+    private bool CheckGrounded()
+    {
+        for(int i = 0; i < GroundPivots.Length; i++)
+        {
+            if (Physics.Raycast(GroundPivots[i].position, Vector3.down, 0.5f))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
