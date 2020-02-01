99f8baf41d8508f8e42eea0a413296ad3a1ba8b7
akifuslu <40760783+akifuslu@users.noreply.github.com>
2020-02-01 16:41:20 +0300

    new levels

---
+++ ggj2020/Assets/Scripts/PlatformMover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMover : MonoBehaviour
+{
+
+    public float Speed;
+    public List<Transform> Waypoints;
+
+    private int _cur = 1;
+
+    private void Start()
+    {
+        transform.position = Waypoints[0].position;
+    }
+
+    private void Update()
+    {
+        var dir = Waypoints[_cur].position - transform.position;
+        dir.Normalize();
+        transform.Translate(dir * Speed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, Waypoints[_cur].position) < .1f)
+        {
+            _cur++;
+            if (_cur == Waypoints.Count) _cur = 0;
+        }
+    }
+}
