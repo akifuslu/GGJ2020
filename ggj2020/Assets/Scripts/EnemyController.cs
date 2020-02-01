98b23db21d557c037ec5025bb1a35f3ab81a9759
akifuslu <40760783+akifuslu@users.noreply.github.com>
2020-02-01 14:44:57 +0300

    Basic enemy ai

---
+++ ggj2020/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float Speed;
+    public bool Fire;
+    public GameObject BoltPrefab;
+    public List<Transform> Waypoints;
+
+    private int _cur;
+    private IDisposable _d;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = Waypoints[0].position;
+        _cur = 1;
+
+        if (Fire)
+        {
+            _d = Observable.Interval(TimeSpan.FromSeconds(3)).Subscribe(ev =>
+            {
+                var go = Instantiate(BoltPrefab, transform.position, transform.rotation);
+                Destroy(go, 1);
+            });
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var dir = Waypoints[_cur].position - transform.position;
+        dir.Normalize();
+        transform.Translate(dir * Speed * Time.deltaTime);
+        if(Vector3.Distance(transform.position, Waypoints[_cur].position) < .1f)
+        {
+            _cur++;
+            if (_cur == Waypoints.Count) _cur = 0;
+        }
+    }
+}
